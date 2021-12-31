eb03ca37cd3dd27d9e09f1b06a4bebb52f36868b
Bassem Yacoube <bassemgeorgi@gmail.com>
2021-12-31 07:41:13 +0000

    conf

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
+is_prod = os.environ.get('IS_HEROKU', None)
 
-API_KEY = os.environ.get('API_KEY')
-API_SECRET = os.environ.get('API_SECRET')
+if is_prod:
+    API_KEY = os.environ.get('API_KEY')
+    API_SECRET = os.environ.get('API_SECRET')
 
-WEBHOOK_PASSPHRASE =  os.environ.get('WEBHOOK_PASSPHRASE')
-DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
+    WEBHOOK_PASSPHRASE =  os.environ.get('WEBHOOK_PASSPHRASE')
+    DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
+else:
+    API_KEY = os.environ.get('API_KEY')
+    API_SECRET = os.environ.get('API_SECRET')
+
+    WEBHOOK_PASSPHRASE =  os.environ.get('WEBHOOK_PASSPHRASE')
+    DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
