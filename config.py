0f7dfd14f8ffe344dba1e832ff60ffc924861ed2
Bassem Yacoube <bassemgeorgi@gmail.com>
2021-12-31 07:33:24 +0000

    envvars

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import os
 
 API_KEY = os.environ.get('API_KEY')
 API_SECRET = os.environ.get('API_SECRET')
 
 WEBHOOK_PASSPHRASE =  os.environ.get('WEBHOOK_PASSPHRASE')
-=======
-
->>>>>>> 5042399f3d7525b8b644486998afd880742a7d59
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
