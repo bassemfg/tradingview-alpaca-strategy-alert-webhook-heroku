d982e3ccd69484dd4e18c5ad678950da8dc8bb34
ML Wiz <bassemgeorgi@gmail.com>
2021-12-31 07:20:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_KEY = 'PKFS6CW7NOLXT670ELOM'
-API_SECRET = 'qqTD4cQdnsIIZ2BcfufcldwjKnonOwrqbYPl9xtF'
+API_KEY = 'asd'
+API_SECRET = 'asd'
 
 WEBHOOK_PASSPHRASE = 'asSDFG345tdvfge56'
 DISCORD_WEBHOOK_URL = False # use a string containing your discord webhook url to enable
