61f93861a7adad5efe2748b09860f962a44003ff
Bassem Yacoube <bassemgeorgi@gmail.com>
2022-01-03 05:52:56 +0000

    sdf

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ def webhook():
 def write_dropbox_message( quantity, price, symbol):
     try:
             
-        dbx = dropbox.Dropbox(config.DB_App1_ACCESS_TOKE,
+        dbx = dropbox.Dropbox(config.DB_App1_ACCESS_TOKEN,
                             app_key=config.db_app_key, app_secret=config.db_app_Secret)
         filename = r'/herokusync/NQ1H_KNN.csv'
         f, r = dbx.files_download(filename)
